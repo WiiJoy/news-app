f90d74a4e7ff3ea40ed6518e55d5f7e38f02428d
Vitaliy (work) <wiijoy@bk.ru>
2021-07-01 11:17:00 +0300

    Add search from form, clear news list, preloader

--- src/js/script.js
+++ src/js/script.js
@@ -78,21 +78,58 @@ document.addEventListener('DOMContentLoaded', function() {
     loadNews(); // вызов, когда прогрузится весь DOM
 });
 
+// Elements
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    loadNews();
+});
+
 
 // функция по загрузке новостей
 function loadNews() {
-    newsService.topHeadlines('ru', onGetResponse);
+    showLoader();
+    const country = countrySelect.value;
+    const searchText = searchInput.value;
+
+    if (!searchText) {
+        newsService.topHeadlines(country, onGetResponse);
+    } else {
+        newsService.everything(searchText, onGetResponse);
+    }
+    
 }
 
 // отработка, когда получаются новости
 function onGetResponse(err, res) {
-    console.log(res);
+
+    removeLoader();
+
+    if (err) {
+        showAlert(err, 'error-msg');
+        return;
+    }
+    // проверка наличия новостей
+    if (!res.articles.length) {
+        // show empty message
+        return;
+    }
+
     renderNews(res.articles);
 }
 
 // рендер получаемых новостей
 function renderNews(news) {
     const newsContainer = document.querySelector('.news-container .row');
+
+    // проверка наполненности контейнера для его очистки
+    if (newsContainer.children.length) {
+        clearContainer(newsContainer);
+    }
+
     let fragment = '';
     news.forEach(newsItem => {
         const el = newsTemplate(newsItem);
@@ -104,7 +141,6 @@ function renderNews(news) {
 
 // формирование разметки каждой новости
 function newsTemplate({urlToImage, title, url, description}) {
-    // console.log(news);
     return `
         <div class="col-s12">
             <div class="card">
@@ -121,4 +157,36 @@ function newsTemplate({urlToImage, title, url, description}) {
             </div>
         </div>
     `;
+}
+
+// выпадающее сообщение при ошибке
+function showAlert(msg, type) {
+    M.toast({html: msg, classes: type}); // параметры из библиотеки
+}
+
+// очистка контейнера новостей
+function clearContainer(container) {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+// прелоадер
+function showLoader() {
+    document.body.insertAdjacentHTML(
+        'afterbegin',
+        `
+            <div class="progress">
+                <div class="indeterminate"></div>
+            </div>
+        `
+    );
+}
+function removeLoader() {
+    const loader = document.querySelector('.progress');
+    if (loader) {
+        loader.remove();
+    }
 }
